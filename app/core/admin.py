27b65a91176b062baea05640c5e0e9f6cb39165d
Yassin Chibrani <y.chibrani@gmail.com>
2024-02-18 00:50:54 +0100

    Added recipe model and test

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,9 +1,10 @@
 """"Django admin customization"""
 
-from core import models
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.utils.translation import gettext_lazy as _
+
+from core import models
 
 
 class UserAdmin(BaseUserAdmin):
@@ -39,3 +40,4 @@ class UserAdmin(BaseUserAdmin):
 
 
 admin.site.register(models.User, UserAdmin)
+admin.site.register(models.Recipe)
